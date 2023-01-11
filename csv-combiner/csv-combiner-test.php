1c3fc75d72c6b1d7c5188d261a501eb1ea6a23fd
hinaman12 <hinaman.12@osu.edu>
2023-01-11 05:10:42 -0500

    unit testing and better csv-combiner

---
+++ csv-combiner/csv-combiner-test.php
@@ -0,0 +1,28 @@
+#!/usr/bin/env phpunit
+<?php
+
+require_once ('PHPUnit/Framework/TestCase.php');
+
+class CSVCombinerTest extends \PHPUnit\Framework\TestCase
+{
+    public function testBasic()
+    {
+        $testFile1 = 'test1.csv';
+        $testFile2 = 'test2.csv';
+        $testFile3 = 'test3.csv';
+        $expectedFile = 'expected.csv';
+
+        file_put_contents($testFile1, "header1,header2\ncontent1_1,content1_2\ncontent2_1,content2_2");
+        file_put_contents($testFile2, "header1,header2\ncontent3_1,content3_2\ncontent4_1,content4_2");
+        file_put_contents($testFile3, "header1,header2\ncontent5_1,content5_2\ncontent6_1,content6_2");
+        file_put_contents($expectedFile, "header1,header2,filename\ncontent1_1,content1_2,test1.csv\ncontent2_1,content2_2,test1.csv\ncontent3_1,content3_2,test2.csv\ncontent4_1,content4_2,test2.csv\ncontent5_1,content5_2,test3.csv\ncontent6_1,content6_2,test3.csv\n");
+
+        $output = shell_exec("./csv-combiner.php $testFile1 $testFile2 $testFile3");
+        $this->assertEquals(file_get_contents($expectedFile), $output);
+
+        unlink($testFile1);
+        unlink($testFile2);
+        unlink($testFile3);
+        unlink($expectedFile);
+    }
+}
--- csv-combiner/csv-combiner.php
+++ csv-combiner/csv-combiner.php
@@ -9,39 +9,33 @@ if ($argc < 2) {
 // Use the first file as the column header
 $headers = array_map('str_getcsv', file($argv[1]));
 
-// Create an array to store the rows
-$rows = array();
-
-// Iterate through the input files
-for ($i = 1; $i < $argc; $i++) {
-    $file = $argv[$i];
-
-    // Open the file for reading
+// Create a generator function
+function read_rows($file) {
     $handle = fopen($file, 'r');
-
-    // Skip the first row (headers)
     fgetcsv($handle);
-
-    // Read the rows from the file
     while (($data = fgetcsv($handle)) !== false) {
         // Add the filename column to the row data
-        array_push($data, basename($file));
-        array_push($rows, $data);
+        $data[] = basename($file);
+        yield $data;
     }
-
-    // Close the file
     fclose($handle);
 }
 
 // Add the new column to headers
-array_push($headers[0], "Filename");
+$headers[0][] = "filename";
 
 // output the csv
 header('Content-Type: text/csv; charset=utf-8');
 header('Content-Disposition: attachment; filename=combined.csv');
 $output = fopen('php://output', 'w');
 fputcsv($output, $headers[0]);
-foreach ($rows as $row) {
-    fputcsv($output, $row);
+
+// iterate through files
+for ($i = 1; $i < $argc; $i++) {
+    $file = $argv[$i];
+    foreach(read_rows($file) as $row)
+    {
+        fputcsv($output, $row);
+    }
 }
 fclose($output);
